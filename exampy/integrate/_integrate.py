1fc1798698dfd3babaac56cd8bf14c10220fff15
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-04 17:52:22 -0500

    Add simple docstring for riemann function

--- exampy/integrate/_integrate.py
+++ exampy/integrate/_integrate.py
@@ -1,4 +1,23 @@
 import numpy as np
 
 def riemann(func,a,b,n=10):
+    """A simple Riemann-sum approximation to the integral of a function
+
+Parameters
+----------
+func: callable
+    Function to integrate, should be a function of one parameter
+a: float
+    Lower limit of the integration range
+b: float
+    Upper limit of the integration range
+n: int, optional
+    Number of intervals to split [a,b] into for the Riemann sum
+
+Returns:
+--------
+float
+    Integral of func(x) over [a,b]
+"""
+
     return np.sum(func(np.linspace(a,b,n))*(b-a)/n)
