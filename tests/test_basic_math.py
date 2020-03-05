b39b1865ef3f849c1b3a9547e7e0adc7a425a28e
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-05 17:31:47 -0500

    First test of basic math in exampy, with error in the test

---
+++ tests/test_basic_math.py
@@ -0,0 +1,14 @@
+# Tests of the basic math functions available in exampy's top level
+
+def test_square_direct():
+    # Direct test that the square works based on known solutions
+    import math
+    import exampy
+    tol = 1e-10
+    assert math.fabs(exampy.square(1.)-1.) < tol, \
+        "exampy.square does not agree with known solution"
+    assert math.fabs(exampy.square(2.)-4.) < tol, \
+        "exampy.square does not agree with known solution"
+    assert math.fabs(exampy.square(3.)-10.) < tol, \
+        "exampy.square does not agree with known solution"
+    return None
