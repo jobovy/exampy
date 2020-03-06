67e3f1b4af626a2fb9083e22dc6359ee3ddd0289
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-05 21:27:33 -0500

    Add test that exampy.integrate.simps agrees with scipy.integrate.quad for a complicated function

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -8,9 +8,25 @@ def test_simps_against_riemann():
     complicated_func= lambda x: x*np.cos(x**2)/(1+np.exp(-x))
     tol= 1e-4
     n_int= 1000
-     assert np.fabs(exampy.integrate.simps(complicated_func,0,1,n=n_int)
+    assert np.fabs(exampy.integrate.simps(complicated_func,0,1,n=n_int)
                    -exampy.integrate.riemann(complicated_func,0,1,n=n_int)) \
                    < tol, \
                    """exampy.integrate.simps gives a different result from """\
                    """exampy.integrate.riemann for a complicated function"""
     return None
+
+def test_simps_against_scipy():
+    # Test that exampy.integrate.simps integration agrees with 
+    # scipy.integrate.quad
+    from scipy import integrate as sc_integrate
+    complicated_func= lambda x: x*np.cos(x**2)/(1+np.exp(-x))
+    tol= 1e-14
+    n_int= 1000
+    assert np.fabs(exampy.integrate.simps(complicated_func,0,1,n=n_int)
+                   -sc_integrate.quad(complicated_func,0,1)[0])\
+                   < tol, \
+                   """exampy.integrate.simps gives a different result from """\
+                   """scipy.integrate.quad for a complicated function"""
+    return None
+
+    
