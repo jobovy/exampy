79b6008b2535f98f44fd6a2fe2da69fe5a290f05
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-14 13:52:40 -0400

    Add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="exampy",
     version="0.2",
     author="Jo Bovy",
     author_email="bovy@astro.utoronto.ca",
     description="A small example Python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["exampy","exampy/integrate"],
     install_requires=["numpy"]
 )
