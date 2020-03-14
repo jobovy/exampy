8f187683aace5cf5794f538024ea287cb786623e
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-14 13:16:32 -0400

    Bump version

--- exampy/__init__.py
+++ exampy/__init__.py
@@ -1,3 +1,3 @@
 """exampy: an example Python package"""
-__version__= "0.1"
+__version__= "0.2"
 from ._math import *
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="exampy",
-    version="0.1",
+    version="0.2",
     author="Jo Bovy",
     author_email="bovy@astro.utoronto.ca",
     description="A small example Python package",
