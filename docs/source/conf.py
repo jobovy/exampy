ec1864d3ed35021c1e280ba7b78828a17d87a1fa
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-18 17:10:30 -0400

    Add sphinx.ext.viewdoc to link to code in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,8 @@ release = '0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc","sphinx.ext.napoleon","nbsphinx"
+extensions = ["sphinx.ext.autodoc","sphinx.ext.napoleon","sphinx.ext.viewcode",
+              "nbsphinx"
 ]
 
 
